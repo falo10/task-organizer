77f15f0a3ccf255bc3a6266a76d915057e2ef243
falo10 <swiecinskiolaf@gmail.com>
2022-06-14 19:08:39 +0200

    Enum as a user menu

--- taskOrganizer.py
+++ taskOrganizer.py
@@ -1,20 +1,23 @@
-
+from enum import IntEnum
 
 menu = """Select one of the following opotions below:
 1) Add new task
 2) View your tasks
 3) Delete a completed task
-
+4) Exit
 Your selection: """
 
 
 print ("""Welcome to Task Organizer by falo10, now You will never forget your tasks at work again!""")
 
+MenuOptions = IntEnum('MenuOption', "Add View Delete Exit")
 
-while ((decision:=input(menu))!= 3):
-	if (decision == "1"):
+while ((decision:=input(menu))!= str(MenuOptions.Exit.value)):
+	if (decision == str(MenuOptions.Add.value)):
 		print ("lets add.....")
-	elif (decision == "2"):
+	elif (decision == str(MenuOptions.View.value)):
 		print ("lets check...")
+	elif (decision == str(MenuOptions.Delete.value)):
+		print ("lets delete...")
 	else:
 		print ("Invalid input! Try again!")
