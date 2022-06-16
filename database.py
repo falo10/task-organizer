e395894bdf3f4cd7b798c22ec096543227f65241
falo10 <swiecinskiolaf@gmail.com>
2022-06-16 17:08:45 +0200

    Create table function update

--- database.py
+++ database.py
@@ -1,3 +1,4 @@
+from email.policy import default
 import sqlite3
 
 connection = sqlite3.connect("databaseTO.db")
@@ -6,16 +7,19 @@ connection.row_factory = sqlite3.Row
 
 def create_table():
     with connection:
-        connection.execute('CREATE TABLE IF NOT EXISTS tasks (task_name text, deadline_date text)')
+        connection.execute('CREATE TABLE IF NOT EXISTS tasks (task_id integer PRIMARY KEY AUTOINCREMENT, task_name text, deadline_date text, comment text, status text)')
 
-def add_task(nameOfTask, completionDate):
+def add_task(nameOfTask, completionDate, comment, status):
     with connection:
-        connection.execute("INSERT INTO tasks VALUES (?, ?);", (nameOfTask, completionDate))
+        connection.execute("INSERT INTO tasks (task_name, deadline_date, comment, status) VALUES (?, ?, ?, ?);", (nameOfTask, completionDate, comment, status))
 
 
 def get_tasks():
     cursor = connection.execute('SELECT * FROM tasks')
     return cursor
+
+
+
     
 
 
