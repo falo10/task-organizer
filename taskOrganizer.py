3ffccc80bd6c2ecb088fbac13d75e6ca910727a0
falo10 <swiecinskiolaf@gmail.com>
2022-06-17 17:21:56 +0200

    getting rid of bugs - ValueErrors

--- taskOrganizer.py
+++ taskOrganizer.py
@@ -15,50 +15,59 @@ Your selection: """
 
 
 def enter_task():
-	nameOfTask = input("Enter the task name:  ")
+	nameOfTask = input("\nEnter the task name:  ")
 	completionDate = input ("Enter date by when the task must be completed: ")
 	comment = input ("Enter the comment: ")
 	database.add_task(nameOfTask, completionDate, comment)
 
 def view_task(tasks):
+	print("\nHere are all your tasks:\n") 
 	for task in tasks:
-		print (f"\n{task['task_id']}\n{task['status']}\n{task['deadline']}\n{task['task_name']}\n{task['comment']}\n\n")
-
+			print (f"\ntask_name: {task[1]}\ntask_id: {task[0]}\nstatus: {task[4]}\ndeadline: {task[2]}\ncomment: {task[3]}\n\n")
+	
 
 def view_to_do_tasks(tasksToDo):
-	print("""
-Here are all your tasks to do:
-			""")
+	print("\nHere are all your tasks:\n") 
 	for task in tasksToDo:
-		print (f"\n{task['deadline']}\n{task['task_name']}\n{task['comment']}\n\n")
+		print (f"\ntask_name: {task[1]}\ntask_id: {task[0]}\nstatus: {task[4]}\ndeadline: {task[2]}\ncomment: {task[3]}\n\n")
 
 def view_completed_tasks(tasksCompleted):
+	print("\nHere are all your tasks:\n") 
 	for task in tasksCompleted:
-		print("""
-Here are all your completed tasks:
-			""")
-		print (f"\n{task['deadline']}\n{task['task_name']}\n{task['comment']}\n\n")
+		print (f"\ntask_name: {task[1]}\ntask_id: {task[0]}\nstatus: {task[4]}\ndeadline: {task[2]}\ncomment: {task[3]}\n\n")
 
 
 def enter_task_to_delete():
-	taskIdToDelete = int(input('Enter the id of the task you want to remove from the list: '))
-	database.delete_task(taskIdToDelete)
+	try:
+		taskIdToDelete = int(input('\nEnter the id of the task you want to remove from the list: '))
+	except ValueError:
+		print ("\nInvalid input! Try again!\n")
+	else:
+		database.delete_task(taskIdToDelete)
 
 
 def enter_task_to_update():
-	idOfTaskToUpdate = int(input("Enter the id of task that you want to update:  "))
-	newCompletionDate = input ("Enter new date by when the task must be completed: ")
-	newComment = input ("Enter new comment: ")
-	database.update(newCompletionDate,newComment,idOfTaskToUpdate)
+	try:
+		idOfTaskToUpdate = int(input("\nEnter the id of task that you want to update:  "))
+		newCompletionDate = input ("Enter new date by when the task must be completed: ")
+		newComment = input ("Enter new comment: ")
+	except ValueError:
+		print ("\nInvalid input! Try again!\n")
+	else:
+		database.update(newCompletionDate,newComment,idOfTaskToUpdate)
 
 def enter_task_to_update_status():
-	taskIdToComplete = int(input('Enter the id of the task you want to change status to COMPLETED: '))
-	database.update_status(taskIdToComplete)
+	try:
+		taskIdToComplete = int(input('\nEnter the id of the task you want to change status to COMPLETED: '))
+	except ValueError:
+		print ("\nInvalid input! Try again!\n")
+	else:
+		database.update_status(taskIdToComplete)
 
 
 database.create_table()
 
-print ("""Welcome to Task Organizer by falo10, now You will never forget your tasks at work again!""")
+print ("Welcome to Task Organizer by falo10, now You will never forget your tasks at work again!\n")
 
 MenuOptions = IntEnum("MenuOption", "Add View To_DO Completed Delete Update Status Exit")
 
@@ -79,4 +88,7 @@ while ((decision:=input(menu))!= str(MenuOptions.Exit.value)):
 	elif (decision == str(MenuOptions.Status.value)):
 		enter_task_to_update_status()
 	else:
-		print ("Invalid input! Try again!")
+		print ("\nInvalid input! Try again!\n")
+
+
+database.close_connection()
